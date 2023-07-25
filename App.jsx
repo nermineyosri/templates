2899dfac2aefa9c0810e13f3f6f2f81d80ff772c
nermine yosri <132491370+nermineyosri@users.noreply.github.com>
2023-07-25 20:53:59 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Nav from "./Components/Nav";
+const App = () => {
+  return <div>
+    
+    <Nav/>
+    </div>
+};
+export default App;
