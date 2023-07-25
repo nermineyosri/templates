16c2573611a592a792b8b67d3fc8d0fb82405297
nermine yosri <132491370+nermineyosri@users.noreply.github.com>
2023-07-25 21:02:55 +0300

    Add files via upload

---
+++ Components/Nav.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+const Nav = () => {
+    return <nav className="text-black">
+        <span>Templates</span>
+    <div>
+        <ul>
+            <li><a href="https://nermineyosri.github.io/leon/"  class="text-blue-700">template 1</a>
+            </li>
+            <li><a href="https://nermineyosri.github.io/kasper/"  class="text-blue-700">template 2</a>
+            </li>
+            <li><a href="https://nermineyosri.github.io/elzero/"  class="text-blue-700">template 3</a>
+            </li>
+            <li><a href="https://nermineyosri.github.io/dashboard/"  class="text-blue-700">template 4</a>
+            </li>
+
+        </ul> 
+    </div>
+    </nav>
+};
+export default Nav;
